9e32b959e7031676fd88b5f178e66c725da7c72a
Luciano Santos <luciano_z7@hotmail.com>
2023-04-07 21:04:35 -0300

    Adding subtests

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Luc")
-	esperado := "Olá, Luc"
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-	}
+	//subtest
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Luc")
+		esperado := "Olá, Luc"
+
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
+
+	//subtest
+	t.Run("diz 'Olá, mundo', quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
+
 }
