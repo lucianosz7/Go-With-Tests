4862b0bd6b6b835541d3b8f26ab8ec3434d652ed
Luciano Santos <luciano_z7@hotmail.com>
2023-04-07 22:15:37 -0300

    Adding more tests for others languages

--- ola.go
+++ ola.go
@@ -3,11 +3,28 @@ package main
 import "fmt"
 
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(nome string) string {
-	return prefixoOlaPortugues + nome
+func Ola(nome string, idioma string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+	return prefixoSaudacao(idioma) + nome
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("Mundo", ""))
+}
+
+func prefixoSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case "espanhol":
+		prefixo = prefixoOlaEspanhol
+	case "francês":
+		prefixo = prefixoOlaFrances
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+	return
 }
--- ola_test.go
+++ ola_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestOla(t *testing.T) {
 
 	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		// t.Helper -> Diz que é um método auxiliar e não será relatado no teste
 		t.Helper()
 		if resultado != esperado {
 			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
@@ -13,15 +14,27 @@ func TestOla(t *testing.T) {
 
 	//subtest
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Luc")
+		resultado := Ola("Luc", "")
 		esperado := "Olá, Luc"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	//subtest
 	t.Run("diz 'Olá, mundo', quando uma string vazia for passada", func(t *testing.T) {
-		resultado := Ola("")
-		esperado := "Olá, mundo"
+		resultado := Ola("", "")
+		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em francês", func(t *testing.T) {
+		resultado := Ola("Elodie", "francês")
+		esperado := "Bonjour, Elodie"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
