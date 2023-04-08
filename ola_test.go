434bb93f294a4b4efb439b574baf61f361d24dd1
Luciano Santos <luciano_z7@hotmail.com>
2023-04-07 21:07:51 -0300

    Refactoring tests

--- ola_test.go
+++ ola_test.go
@@ -4,24 +4,25 @@ import "testing"
 
 func TestOla(t *testing.T) {
 
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
+
 	//subtest
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
 		resultado := Ola("Luc")
 		esperado := "Olá, Luc"
-
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	//subtest
 	t.Run("diz 'Olá, mundo', quando uma string vazia for passada", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá, mundo"
-
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 }
